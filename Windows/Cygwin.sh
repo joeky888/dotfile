71638ecebb2b94fb89201a3ae182a2671835878a
j16180339887 <jj16180339887@gmail.com>
2016-05-18 20:55:24 +0800

    Cygwin install lxde

--- Windows/Cygwin.sh
+++ Windows/Cygwin.sh
@@ -4,3 +4,11 @@ $ apt-cyg install wget curl tar p7zip git openssh sed wget vim tmux zsh make aut
 $ echo "/cygdrive/c/Users /home none bind 0 0" >> /etc/fstab
 $ curl -LOC - "https://cygwin.com/setup-x86_64.exe" && install setup-x86_64.exe /bin && rm setup-x86_64.exe
 $ alias upgradeCygwin="setup-x86_64.exe --no-desktop --no-shortcuts --no-startmenu --quiet-mode"
+
+# Install GUI desktop environment
+$ apt-cyg install xorg-server xinit lxde-common
+$ XWin :0 -clipboard -multiwindow
+$ DISPLAY=:0.0 xterm
+$ startlxde
+# OR
+$ DISPLAY=:0.0 startlxde
