579a8cd22448e6c31c85d05ae1371f1cf95f145d
j16180339887 <jj16180339887@gmail.com>
2016-06-27 10:17:36 +0800

    Drop find command

--- FFmpegConverter.py
+++ FFmpegConverter.py
@@ -4,16 +4,17 @@
 # $ ./FFmpegConverter.py wav mp3
 # $ python3 FFmpegConverter.py wav mp3
 
-import subprocess, sys, os, codecs, time
+import sys, os, codecs, time, fnmatch
 
 if len(sys.argv) == 1:
     print("Usage: ./FFmpegConverter.py <format> <format>")
     sys.exit(0)
 
-try:
-    Files = subprocess.check_output("find . -iname \"*." + sys.argv[1] + "\"", shell=True).splitlines()
-except subprocess.CalledProcessError as e:
-    print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+Files = []
+
+for root, dirnames, filenames in os.walk("."):
+    for filename in fnmatch.filter(filenames, "*." + sys.argv[1]):
+        Files.append(os.path.join(root, filename))
 
 outdir = "../" + os.path.relpath(".","..") + time.strftime("-%Y-%m-%d-%H-%M-%S") + "/"
 Error_List = []
@@ -26,7 +27,7 @@ def run(commands, file):
 
 for file in Files:
     # ffmpeg -i "$file" "${file%.wav}".mp3
-    file = file.decode(encoding='UTF-8')
+    # file = file.decode(encoding='UTF-8')
     commands = []
     target = "\"" + outdir + os.path.splitext(file)[0][2:] + "." + sys.argv[2] + "\""
     targetFolder = "\"" + os.path.dirname(outdir + file[2:]) + "\""
