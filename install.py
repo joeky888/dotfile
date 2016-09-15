2934c340194494e111bfca7088adacdff6fb42dc
j16180339887 <jj16180339887@gmail.com>
2016-09-15 08:08:30 +0800

    Remove apt-cyg

--- install.py
+++ install.py
@@ -14,13 +14,13 @@ if len(sys.argv) == 1:
 commands = []    
 
 def windows():
-    commands.append("lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg");
-    commands.append("install apt-cyg /bin && rm apt-cyg");
-    commands.append("apt-cyg install wget curl tar p7zip git openssh sed wget vim tmux zsh fontconfig ghostscript make automake cmake gcc-core gcc-g++");
-    commands.append("apt-cyg install openssl-devel libevent-devel libncurses-devel libncursesw-devel libtool yasm yasm-devel binutils diffutils dos2unix libfontconfig-devel libiconv-devel libass-devel fribidi libfribidi-devel libfreetype-devel libopenjpeg-devel libopus-devel libvorbis-devel libvpx-devel libwebp-devel libbz2-devel");
+    #commands.append("lynx -source rawgit.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg");
+    #commands.append("install apt-cyg /bin && rm apt-cyg");
+    commands.append("curl -LOC - 'https://cygwin.com/setup-x86_64.exe' && install setup-x86_64.exe /bin && rm setup-x86_64.exe");
+    commands.append("setup-x86_64.exe --no-desktop --no-shortcuts --no-startmenu --quiet-mode --wait --packages wget curl tar p7zip git openssh sed wget vim tmux zsh fontconfig ghostscript make automake cmake gcc-core gcc-g++");
+    commands.append("setup-x86_64.exe --no-desktop --no-shortcuts --no-startmenu --quiet-mode --wait --packages openssl-devel libevent-devel libncurses-devel libncursesw-devel libtool yasm yasm-devel binutils diffutils dos2unix libfontconfig-devel libiconv-devel libass-devel fribidi libfribidi-devel libfreetype-devel libopenjpeg-devel libopus-devel libvorbis-devel libvpx-devel libwebp-devel libbz2-devel");
     commands.append("grep -q -F '/cygdrive/c/Users /home none bind 0 0' /etc/fstab || echo '/cygdrive/c/Users /home none bind 0 0' >> /etc/fstab");
     commands.append("mount -a");
-    commands.append("curl -LOC - 'https://cygwin.com/setup-x86_64.exe' && install setup-x86_64.exe /bin && rm setup-x86_64.exe");
     commands.append("rm -rf ~/dotfile")
     commands.append("rm -rf ~/.oh-my-zsh")
     commands.append("git clone --depth=1 https://github.com/j16180339887/dotfile.git ~/dotfile");
