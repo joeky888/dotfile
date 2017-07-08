3c616253fb7732f8c48d41bd8994143523b7c11b
j16180339887 <jj16180339887@gmail.com>
2017-07-08 14:07:15 +0800

    Remove old files

--- Web/Crawlers/curl.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import subprocess, sys, os, codecs
-
-i = 1
-while i <= 75:
-    while os.system("curl -LOC - http://hkdbchannel.felixwongsite.com/video/" + str(i) + ".mp4") != 8448:
-        print ("curl -LOC - http://hkdbchannel.felixwongsite.com/video/" + str(i) + ".mp4")
-    i+=1
