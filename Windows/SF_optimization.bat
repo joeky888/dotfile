0e471e917b47dc5105e2771c3d6406c70e0bff4d
j16180339887 <jj16180339887@gmail.com>
2017-07-26 16:04:01 +0800

    Lower the mtu value

--- Windows/SF_optimization.bat
+++ Windows/SF_optimization.bat
@@ -19,3 +19,6 @@ del /f /s /q "%ProgramFiles(x86)%\WAYI\SFOnline\XIGNCODE\*"
 del /f /s /q "%ProgramFiles(x86)%\WAYI\SFOnline\SpecialForce_exe_log.edl"
 
 break>"%ProgramFiles(x86)%\WAYI\SFOnline\data\lobby\SF_Curse.drk"
+
+:: Lower the mtu value
+netsh interface ipv4 set subinterface Wi-Fi mtu=296  store=persistent
