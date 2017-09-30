817124bab9038a5b622838cba89633b8509f4099
j16180339887 <jj16180339887@gmail.com>
2017-10-01 07:18:19 +0800

    Revert

--- install.sh
+++ install.sh
@@ -87,7 +87,7 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then # Cygwin
   aria2c 'https://cygwin.com/setup-x86_64.exe' && install setup-x86_64.exe /bin && rm setup-x86_64.exe
   apt-cyg install wget curl aria2 tar p7zip git tig openssh vim nano tmux zsh bind-utils
   rm -rf $Home/dotfile/
-  git clone --depth=1 git://github.com/j16180339887/dotfile.git $Home/dotfile
+  git clone --depth=1 https://github.com/j16180339887/dotfile.git $Home/dotfile
   cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.bashrc"
   cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.bash_profile"
   cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.tmux.conf"
@@ -121,7 +121,7 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then # Cygwin
   apt-cyg install cygwin-devel doxygen python3-devel openssl-devel libevent-devel libncurses-devel libncursesw-devel libtool yasm yasm-devel binutils diffutils dos2unix libfontconfig-devel libiconv-devel libass-devel fribidi libfribidi-devel libfreetype-devel libopenjpeg-devel libopus-devel libvorbis-devel libvpx-devel libwebp-devel libbz2-devel libffi-devel gettext-devel
   cd $Home/dotfile && git submodule update --init --recursive
 
-  git clone --depth 1 git://github.com/garabik/grc.git $Home/grc
+  git clone --depth 1 https://github.com/garabik/grc.git $Home/grc
   cd $Home/grc && bash install.sh && cd $Home && rm -rf $Home/grc
 python3 <<END
 import sys, os, site
