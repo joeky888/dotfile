480e4750f4404f7117b8ae951d4f6da9b763758f
j16180339887 <jj16180339887@gmail.com>
2017-10-01 18:42:09 +0800

    Make it shorter

--- install.sh
+++ install.sh
@@ -2,25 +2,90 @@
 
 export SUDO=''
 if (( $EUID != 0 )); then
-    export SUDO='sudo'
+  export SUDO='sudo'
 fi
 
+InstallDotfile()
+{
+  rm -rf $Home/dotfile
+  git clone --depth=1 https://github.com/j16180339887/dotfile.git $Home/dotfile
+  ln -sf $Home/dotfile/.bashrc ~/.bashrc
+  ln -sf $Home/dotfile/.bashrc ~/.bash_profile
+  ln -sf $Home/dotfile/.tmux.conf ~/.tmux.conf
+  ln -sf $Home/dotfile/.bashrc ~/.zshrc
+  ln -sf $Home/dotfile/.vimrc ~/.vimrc
+  ln -sf $Home/dotfile/.gitconfig ~/.gitconfig
+  ln -sf $Home/dotfile/.fbtermrc ~/.fbtermrc
+  ln -sf $Home/dotfile/.Xresources ~/.Xresources
+  cd $Home/dotfile
+  git submodule update --init --recursive
+}
+
+InstallDotfileCygwin()
+{
+  rm -rf $Home/dotfile
+  git clone --depth=1 https://github.com/j16180339887/dotfile.git $Home/dotfile
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.bashrc"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.bash_profile"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.tmux.conf"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.zshrc"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.minttyrc"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.vimrc"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.gitconfig"
+  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.Xresources"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.bashrc" "%USERPROFILE%\dotfile\.bashrc"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.bash_profile" "%USERPROFILE%\dotfile\.bashrc"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.tmux.conf" "%USERPROFILE%\dotfile\.tmux.conf"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.zshrc" "%USERPROFILE%\dotfile\.bashrc"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.minttyrc" "%USERPROFILE%\dotfile\Windows\.minttyrc"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.vimrc" "%USERPROFILE%\dotfile\.vimrc"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\dotfile\.gitconfig"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.Xresources" "%USERPROFILE%\dotfile\.Xresources"
+  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\Documents\WindowsPowerShell\profile.ps1" "%USERPROFILE%\dotfile\Windows\profile.ps1"
+  cd $Home/dotfile
+  git submodule update --init --recursive
+}
+
+InstallGRC()
+{
+  git clone --depth 1 https://github.com/garabik/grc.git grc && cd grc && $SUDO zsh install.sh && cd .. && rm -rf grc
+}
+
+InstallPy3UTF8()
+{
+  python3 <<END
+import sys, os, site
+
+pkgLocation = site.getsitepackages()
+for location in pkgLocation:
+  os.system(os.environ['SUDO'] + " install -D " + os.environ['Home'] + "/dotfile/Windows/sitecustomize.py " + location + "/sitecustomize.py")
+END
+}
+
+InstallPIP()
+{
+  curl 'https://bootstrap.pypa.io/get-pip.py' | $SUDO python3
+  echo y | $SUDO pip install youtube-dl
+  echo y | $SUDO pip install you-get
+  echo y | $SUDO pip install bypy
+}
+
+InstallMinicondaLinux()
+{
+  rm -rf ~/Miniconda3 && aria2c 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh' && chmod 777 Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh -p ~/Miniconda3 -b -f && rm Miniconda3-latest-Linux-x86_64.sh
+  echo y | ~/Miniconda3/bin/pip install youtube-dl
+  echo y | ~/Miniconda3/bin/pip install you-get
+  echo y | ~/Miniconda3/bin/pip install bypy
+}
+
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then # Ubuntu
+  export Home="$HOME"
   $SUDO apt update
   $SUDO apt install tmux zsh git tig curl wget aria2 dnsutils python3 -y
-  rm -rf ~/dotfile
-  git clone --depth=1 https://github.com/j16180339887/dotfile.git ~/dotfile
-  cd ~/dotfile
-  git submodule update --init --recursive
-  ln -sf ~/dotfile/.bashrc ~/.bashrc
-  ln -sf ~/dotfile/.bashrc ~/.bash_profile
-  ln -sf ~/dotfile/.tmux.conf ~/.tmux.conf
-  ln -sf ~/dotfile/.bashrc ~/.zshrc
-  ln -sf ~/dotfile/.vimrc ~/.vimrc
-  ln -sf ~/dotfile/.gitconfig ~/.gitconfig
-  ln -sf ~/dotfile/.fbtermrc ~/.fbtermrc
-  ln -sf ~/dotfile/.Xresources ~/.Xresources
+
   mkdir -p ~/.config/openbox
+  InstallDotfile
   ln -sf ~/dotfile/Linux/.config_openbox_rc.xml ~/.config/openbox/rc.xml
   ln -sf ~/dotfile/Linux/.config_openbox_rc.xml ~/.config/openbox/lxqt-rc.xml
   ln -sf ~/dotfile/Linux/.config_openbox_rc.xml ~/.config/openbox/lxde-rc.xml
@@ -42,36 +107,23 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then # Ubuntu
   aria2c http://font.ubuntu.com/download/ubuntu-font-family-0.83.zip && 7z -o"ubuntu" x ubuntu-font-family-0.83.zip && $SUDO mv -v ubuntu /usr/share/fonts/truetype/Ubuntu && rm ubuntu-font-family*
   fc-cache -fv
   find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; > ~/.nanorc
-  rm -rf ~/Miniconda3 && aria2c 'https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh' && chmod 777 Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh -p ~/Miniconda3 -b -f && rm Miniconda3-latest-Linux-x86_64.sh
-  echo y | ~/Miniconda3/bin/pip install youtube-dl
-  echo y | ~/Miniconda3/bin/pip install you-get
-  echo y | ~/Miniconda3/bin/pip install bypy
-  git clone --depth 1 https://github.com/garabik/grc.git grc && cd grc && $SUDO sh install.sh && cd .. && rm -rf grc
-python3 <<END
-import sys, os, site
+  InstallMinicondaLinux
+  InstallGRC
+  InstallPy3UTF8
 
-pkgLocation = site.getsitepackages()
-for location in pkgLocation:
-  os.system(os.environ['SUDO'] + " install -D ~/dotfile/Windows/sitecustomize.py " + location + "/sitecustomize.py")
-END
 
 
 
 
 
 elif [[ "$OSTYPE" == "darwin"* ]]; then # Mac OSX
+  export SUDO=''
+  export Home="$HOME"
   alias ls='ls -G'
-  rm -rf ~/dotfile
-  git clone --depth=1 https://github.com/j16180339887/dotfile.git ~/dotfile
-  cd ~/dotfile
-  git submodule update --init --recursive
-  ln -sf ~/dotfile/.bashrc ~/.bashrc
-  ln -sf ~/dotfile/.bashrc ~/.bash_profile
-  ln -sf ~/dotfile/.tmux.conf ~/.tmux.conf
-  ln -sf ~/dotfile/.bashrc ~/.zshrc
-  ln -sf ~/dotfile/.vimrc ~/.vimrc
-  ln -sf ~/dotfile/.gitconfig ~/.gitconfig
-  ln -sf ~/dotfile/.Xresources ~/.Xresources
+  InstallDotfile
+  InstallPIP
+  InstallPy3UTF8
+  InstallGRC
 
 
 
@@ -81,30 +133,13 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then # Cygwin
     echo "Please install aria2"
     exit 1
   fi
+  export SUDO=''
   export Home=$(cygpath -u "$USERPROFILE")
   curl https://raw.githubusercontent.com/j16180339887/apt-cyg/master/apt-cyg > apt-cyg
   install apt-cyg /bin && rm apt-cyg
   aria2c 'https://cygwin.com/setup-x86_64.exe' && install setup-x86_64.exe /bin && rm setup-x86_64.exe
   apt-cyg install wget curl aria2 tar p7zip git tig openssh vim nano tmux zsh bind-utils
-  rm -rf $Home/dotfile/
-  git clone --depth=1 https://github.com/j16180339887/dotfile.git $Home/dotfile
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.bashrc"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.bash_profile"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.tmux.conf"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.zshrc"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.minttyrc"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.vimrc"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.gitconfig"
-  cygstart --action=runas cmd.exe /c del "%USERPROFILE%\.Xresources"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.bashrc" "%USERPROFILE%\dotfile\.bashrc"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.bash_profile" "%USERPROFILE%\dotfile\.bashrc"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.tmux.conf" "%USERPROFILE%\dotfile\.tmux.conf"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.zshrc" "%USERPROFILE%\dotfile\.bashrc"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.minttyrc" "%USERPROFILE%\dotfile\Windows\.minttyrc"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.vimrc" "%USERPROFILE%\dotfile\.vimrc"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\dotfile\.gitconfig"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\.Xresources" "%USERPROFILE%\dotfile\.Xresources"
-  cygstart --action=runas cmd.exe /c mklink "%USERPROFILE%\Documents\WindowsPowerShell\profile.ps1" "%USERPROFILE%\dotfile\Windows\profile.ps1"
+  InstallDotfileCygwin
   cygstart --action=runas cmd.exe /c RD /S /Q "%ALLUSERSPROFILE%\\chocolatey"
   cygstart --action=runas cmd.exe /c @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
   [[ "$PATH" != *"chocolatey/bin"* ]] && cygstart --action=runas cmd.exe /c "setlocal EnableDelayedExpansion & setx /M PATH \"%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin\""
@@ -112,112 +147,55 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then # Cygwin
   grep -q -F 'none /tmp usertemp binary,posix=0 0 0' /etc/fstab || echo 'none /tmp usertemp binary,posix=0 0 0' >> /etc/fstab
   sed -i 's/.*db_shell.*/db_shell: \/bin\/zsh/' /etc/nsswitch.conf
   find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; > ~/.nanorc
-  curl 'https://bootstrap.pypa.io/get-pip.py' | python3
-  echo y | pip install youtube-dl
-  echo y | pip install you-get
-  echo y | pip install bypy
 
   apt-cyg install procps fontconfig fontforge ghostscript ImageMagick make automake cmake gcc-core gcc-g++
   apt-cyg install cygwin-devel doxygen python3-devel openssl-devel libevent-devel libncurses-devel libncursesw-devel libtool yasm yasm-devel binutils diffutils dos2unix libfontconfig-devel libiconv-devel libass-devel fribidi libfribidi-devel libfreetype-devel libopenjpeg-devel libopus-devel libvorbis-devel libvpx-devel libwebp-devel libbz2-devel libffi-devel gettext-devel
-  cd $Home/dotfile && git submodule update --init --recursive
+  InstallPIP
+  InstallGRC
+  InstallPy3UTF8
 
-  git clone --depth 1 https://github.com/garabik/grc.git $Home/grc
-  cd $Home/grc && bash install.sh && cd $Home && rm -rf $Home/grc
-python3 <<END
-import sys, os, site
-
-pkgLocation = site.getsitepackages()
-for location in pkgLocation:
-  os.system("install -D " + os.environ['Home'] + "/dotfile/Windows/sitecustomize.py " + location + "/sitecustomize.py")
-END
 
 
 
 
 
 elif [[ "$OSTYPE" == "msys" ]]; then # Msys
+  export SUDO=''
+  export Home="$HOME"
   pacman -Syu
   pacman -S zsh git vim curl wget base-devel msys2-devel mercurial cvs p7zip perl ruby python2 python3 mingw-w64-x86_64-toolchain
-  curl 'https://bootstrap.pypa.io/get-pip.py' | python3
-  echo y | pip install youtube-dl
-  echo y | pip install you-get
-  echo y | pip install bypy
-python3 <<END
-import sys, os, site
-
-pkgLocation = site.getsitepackages()
-for location in pkgLocation:
-  os.system("install -D ~/dotfile/Windows/sitecustomize.py " + location + "/sitecustomize.py")
-END
+  InstallPIP
+  InstallGRC
+  InstallPy3UTF8
 
 
 
 
 
 elif [[ "$OSTYPE" == "linux-android" ]]; then # Android Termux
+  export SUDO=''
+  export Home="$HOME"
   apt update && apt upgrade
   termux-setup-storage
   apt install -y clang man vim git tig zsh tmux curl aria2 openssh grep sed python ffmpeg p7zip util-linux neofetch python-dev libffi-dev openssl-dev
-  rm -rf ~/dotfile
-  git clone --depth=1 https://github.com/j16180339887/dotfile.git ~/dotfile
-  cd ~/dotfile
-  git submodule update --init --recursive
-  ln -sf ~/dotfile/.bashrc ~/.bashrc
-  ln -sf ~/dotfile/.bashrc ~/.bash_profile
-  ln -sf ~/dotfile/.tmux.conf ~/.tmux.conf
-  ln -sf ~/dotfile/.bashrc ~/.zshrc
-  ln -sf ~/dotfile/.vimrc ~/.vimrc
-  ln -sf ~/dotfile/.gitconfig ~/.gitconfig
-  curl 'https://bootstrap.pypa.io/get-pip.py' | python3
-  echo y | pip install youtube-dl
-  echo y | pip install you-get
-  echo y | pip install bypy
-
-  python3 <<END
-import sys, os, site
-
-pkgLocation = site.getsitepackages()
-for location in pkgLocation:
-  os.system("install -D ~/dotfile/Windows/sitecustomize.py " + location + "/sitecustomize.py")
-END
+  InstallDotfile
+  InstallPIP
+  InstallPy3UTF8
 
 
 
 
 
 elif [[ "$OSTYPE" == "freebsd"* ]]; then # FreeBSD or TrueOS
+  export Home="$HOME"
   $SUDO pkg update
   echo y | $SUDO pkg install tmux zsh git vim nano curl python3 ubuntu-font
   chsh -s $(command -v zsh) $(whoami)
   find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; > ~/.nanorc
-  rm -rf ~/dotfile
-  git clone --depth=1 https://github.com/j16180339887/dotfile.git ~/dotfile
-  cd ~/dotfile
-  git submodule update --init --recursive
-  ln -sf ~/dotfile/.bashrc ~/.bashrc
-  ln -sf ~/dotfile/.bashrc ~/.bash_profile
-  ln -sf ~/dotfile/.tmux.conf ~/.tmux.conf
-  ln -sf ~/dotfile/.bashrc ~/.zshrc
-  ln -sf ~/dotfile/.vimrc ~/.vimrc
-  ln -sf ~/dotfile/.gitconfig ~/.gitconfig
-  ln -sf ~/dotfile/.Xresources ~/.Xresources
-  curl 'https://bootstrap.pypa.io/get-pip.py' | $SUDO python3
-  echo y | $SUDO pip install youtube-dl
-  echo y | $SUDO pip install you-get
-  echo y | $SUDO pip install bypy
-
-  git clone --depth 1 https://github.com/garabik/grc.git grc
-  cd grc
-  $SUDO zsh install.sh
-  cd ..
-  rm -rf grc
-python3 <<END
-import sys, os, site
-
-pkgLocation = site.getsitepackages()
-for location in pkgLocation:
-  os.system(os.environ['SUDO'] + " install -D ~/dotfile/Windows/sitecustomize.py " + location + "/sitecustomize.py")
-END
+  InstallDotfile
+  InstallPIP
+  InstallGRC
+  InstallPy3UTF8
 
 
 
