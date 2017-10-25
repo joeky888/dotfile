6e95b72519a8709a401cae6defd195e1d4c73a21
j16180339887 <jj16180339887@gmail.com>
2017-10-25 08:55:38 +0800

    Revert

--- Windows/profile.ps1
+++ Windows/profile.ps1
@@ -106,10 +106,10 @@ Function aria2c {
   Invoke-Expression "aria2c.exe $env:DOWNLOADARGS '$args'"
 }
 Function youtube-dl {
-  youtube-dl.exe -f "bestvideo[height<=1080][fps<=30]+bestaudio/best" --write-sub --sub-lang zh-TW,zh-Hant,zh-CN,zh-Hans,en,enUS,English --ignore-errors --external-downloader aria2c --external-downloader-args $env:DOWNLOADARGS '$args'
+  youtube-dl.exe -f "bestvideo[height<=1080][fps<=30]+bestaudio/best" --write-sub --sub-lang zh-TW,zh-Hant,zh-CN,zh-Hans,en,enUS,English --ignore-errors --external-downloader aria2c --external-downloader-args $env:DOWNLOADARGS $args
 }
 Function youtube-dl-mp3 {
-  youtube-dl.exe --extract-audio --audio-format mp3 --write-sub --sub-lang zh-TW,zh-Hant,zh-CN,zh-Hans,en,enUS,English --ignore-errors --external-downloader aria2c --external-downloader-args $env:DOWNLOADARGS '$args'
+  youtube-dl.exe --extract-audio --audio-format mp3 --write-sub --sub-lang zh-TW,zh-Hant,zh-CN,zh-Hans,en,enUS,English --ignore-errors --external-downloader aria2c --external-downloader-args $env:DOWNLOADARGS $args
 }
 
 # Add chocolatey packages to path
