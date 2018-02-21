ac338d3796b7310cafda4e7fe705bcac71ed9c5d
Joeky Zhan <jj16180339887@gmail.com>
2018-02-21 20:33:46 +0800

    +Virtualbox gaming
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

---
+++ Windows/HideVirtualBox.bat
@@ -0,0 +1,6 @@
+@echo off
+@reg copy HKLM\HARDWARE\ACPI\DSDT\VBOX__ HKLM\HARDWARE\ACPI\DSDT\NOBOX__ /s /f
+@reg delete HKLM\HARDWARE\ACPI\DSDT\VBOX__ /f
+@reg add HKLM\HARDWARE\DESCRIPTION\System /v SystemBiosVersion /t REG_MULTI_SZ /d "NOBOX - 1" /f
+@reg add HKLM\HARDWARE\DESCRIPTION\System /v VideoBiosVersion /t REG_MULTI_SZ /d "" /f
+@exit
