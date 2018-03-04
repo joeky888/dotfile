efc841b32849c832550d647fc046c47dd2f2c9b6
Joeky Zhan <jj16180339887@gmail.com>
2018-03-04 12:06:42 +0800

    Auto rename
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- Web/Crawlers/bili.py
+++ Web/Crawlers/bili.py
@@ -8,16 +8,16 @@ Error_List = []
 url = "https://www.bilibili.com/video/av9760383/#page="
 
 for i in range(1, 21):
-    if os.system("you-get " + url + str(i)) != 0:
-        print("Download Error: " + url + str(i))
-        Error_List.append(url + str(i))
+    if os.system("you-get " + url + str(i) + " -O " + str(i) + ".mp4") != 0:
+        print("Download Error: " + str(i))
+        Error_List.append(i)
 
 # Retry
 while len(Error_List) != 0:
     NewError_List = []
-    for u in Error_List:
-        if os.system("you-get " + u) != 0:
-            print("Download Error: " + u)
-            NewError_List.append(u)
+    for i in Error_List:
+        if os.system("you-get " + url + str(i) + " -O " + str(i) + ".mp4") != 0:
+            print("Download Error: " + str(i))
+            NewError_List.append(i)
     Error_List = []
     Error_List = NewError_List[:]
