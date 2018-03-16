4377a4dbad22233cb6ff7329ed2f511343febd56
Joeky Zhan <jj16180339887@gmail.com>
2018-03-16 11:14:36 +0800

    Retry if fails again
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- Web/Crawlers/bili.py
+++ Web/Crawlers/bili.py
@@ -2,27 +2,24 @@
 
 import subprocess, sys, os, codecs, time
 
-Error_List = []
 MAX_DOWNLOAD_AT_ONCE = 10
 processes = []
-url = "https://www.bilibili.com/video/av8927491/?p="
+url = "https://www.bilibili.com/video/av7931969/?p="
 start = 1
-end   = 25
+end   = 140
 
 for i in range(start, end + 1):
     processes.append( subprocess.Popen(["you-get", url + str(i), "-O", str(i), "-o", str(i)]) )
     while len(processes) >= MAX_DOWNLOAD_AT_ONCE:
         time.sleep(.1)
-        for i in range(len(processes)-1, -1, -1):
-            if processes[i].poll() == 0:
-                processes.pop(i)
-            elif processes[i].returncode:
-                Error_List.append(processes.pop(i))
-
-        processes.extend(Error_List)
-        Error_List = []
-
+        for j in range(len(processes)-1, -1, -1):
+            if processes[j].poll() == 0:
+                # Succeed
+                processes.pop(j)
+            elif processes[j].returncode != None:
+                # Failed
+                processes.pop(j)
+                processes.append( subprocess.Popen(["you-get", url + str(j), "-O", str(j), "-o", str(j)]) )
 
 # Execute this command
 # $ find . -name '*' -type f -exec mv {} ./ \;
-
