a6ae024b465a75701bc588d1221db57f2e5ff7b5
Joeky Zhan <jj16180339887@gmail.com>
2018-03-16 09:54:22 +0800

    Retry if error occurs
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- Web/Crawlers/bili.py
+++ Web/Crawlers/bili.py
@@ -4,17 +4,25 @@ import subprocess, sys, os, codecs, time
 
 Error_List = []
 MAX_DOWNLOAD_AT_ONCE = 10
-processes = set()
+processes = []
 url = "https://www.bilibili.com/video/av8927491/?p="
 start = 1
 end   = 25
 
-for i in range(start, end+1):
-    processes.add( subprocess.Popen(["you-get", url + str(i), "-O", str(i), "-o", str(i)]) )
+for i in range(start, end + 1):
+    processes.append( subprocess.Popen(["you-get", url + str(i), "-O", str(i), "-o", str(i)]) )
     while len(processes) >= MAX_DOWNLOAD_AT_ONCE:
         time.sleep(.1)
-        processes.difference_update([
-            p for p in processes if p.poll() is not None])
+        for i in range(len(processes)-1, -1, -1):
+            if processes[i].poll() == 0:
+                processes.pop(i)
+            elif processes[i].returncode:
+                Error_List.append(processes.pop(i))
+
+        processes.extend(Error_List)
+        Error_List = []
+
 
 # Execute this command
 # $ find . -name '*' -type f -exec mv {} ./ \;
+
