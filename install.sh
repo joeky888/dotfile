6a2ebfca6cbca0b1400b87f551f4d1a01aabec46
Joeky Zhan <jj16180339887@gmail.com>
2018-05-24 23:25:08 +0800

    +openssl
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- install.sh
+++ install.sh
@@ -193,7 +193,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     $SUDO apt-fast install libssl-dev -y
   elif [[ $(command -v pacman) ]]; then
     pac=$([ -f /usr/bin/powerpill ] && echo "powerpill" || echo "pacman")
-    $SUDO $pac -S p7zip base-devel command-not-found nano ffmpeg neofetch fontconfig traceroute glances net-tools iproute2 bind-tools rsync python3 wget tig htop --noconfirm --needed
+    $SUDO $pac -S p7zip base-devel command-not-found nano ffmpeg neofetch fontconfig traceroute glances openssl net-tools iproute2 bind-tools rsync python3 wget tig htop --noconfirm --needed
   fi
 
   $SUDO aria2c https://raw.githubusercontent.com/j16180339887/CJK-font/master/DroidSansFallback.ttf --dir=/ -o usr/share/fonts/truetype/DroidSansFallback.ttf
