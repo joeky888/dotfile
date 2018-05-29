fcc66c57f88add545ebe9b5e427ca4ccb81c4607
Joeky Zhan <jj16180339887@gmail.com>
2018-05-29 13:38:25 +0800

    +public.sh
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

---
+++ public.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Transfer all personal settings to public
+# Usefull when I don't own this machine
+
+sed -i '/template/d'   ~/.gitconfig
+sed -i '/name/d'       ~/.gitconfig
+sed -i '/email/d'      ~/.gitconfig
+
+sed -i '/update_prompt/d'                   ~/.zshrc
+sed -i 's/ZSH_THEME=""/ZSH_THEME="bira"'    ~/.zshrc
+
