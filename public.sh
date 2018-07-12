2b16f3eac0ac140ef675521a1569b3bf1c426067
Joeky Zhan <jj16180339887@gmail.com>
2018-07-12 10:17:04 +0800

    Update perl command
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- public.sh
+++ public.sh
@@ -3,21 +3,21 @@
 # Transfer all personal settings to public
 # Usefull when I don't own this machine
 
-perl -C -Mutf8 -pi -e '/template/d' ~/dotfile/.gitconfig
-perl -C -Mutf8 -pi -e '/name/d'     ~/dotfile/.gitconfig
-perl -C -Mutf8 -pi -e '/email/d'    ~/dotfile/.gitconfig
+# perl -C -Mutf8 -pi -e '/template/d' ~/dotfile/.gitconfig
+# perl -C -Mutf8 -pi -e '/name/d'     ~/dotfile/.gitconfig
+# perl -C -Mutf8 -pi -e '/email/d'    ~/dotfile/.gitconfig
 
-perl -C -Mutf8 -ne 'print if !/template/' ~/dotfile/.gitconfig > ~/dotfile/.gitconfig
-perl -C -Mutf8 -ne 'print if !/name/'     ~/dotfile/.gitconfig > ~/dotfile/.gitconfig
-perl -C -Mutf8 -ne 'print if !/email/'    ~/dotfile/.gitconfig > ~/dotfile/.gitconfig
+perl -C -Mutf8 -ni -e 'print if !/template/' ~/dotfile/.gitconfig > ~/dotfile/.gitconfig
+perl -C -Mutf8 -ni -e 'print if !/name/'     ~/dotfile/.gitconfig > ~/dotfile/.gitconfig
+perl -C -Mutf8 -ni -e 'print if !/email/'    ~/dotfile/.gitconfig > ~/dotfile/.gitconfig
 
 # sed -i $backup '/template/d'   ~/dotfile/.gitconfig
 # sed -i $backup '/name/d'       ~/dotfile/.gitconfig
 # sed -i $backup '/email/d'      ~/dotfile/.gitconfig
 # sed -i $backup "/pageup/Id"    ~/.vimrc
 # sed -i $backup "/pagedown/Id"  ~/.vimrc
-perl -C -Mutf8 -ne 'print if !/pageup/i'    ~/.vimrc > ~/.vimrc
-perl -C -Mutf8 -ne 'print if !/pagedown/i'  ~/.vimrc > ~/.vimrc
+perl -C -Mutf8 -ni -e 'print if !/pageup/i'    ~/.vimrc
+perl -C -Mutf8 -ni -e 'print if !/pagedown/i'  ~/.vimrc
 # perl -C -Mutf8 -pi -e "/pageup/id"    ~/.vimrc
 # perl -C -Mutf8 -pi -e "/pagedown/id"  ~/.vimrc
 # sed -i '/zsh-hook/d'                        ~/dotfile/.bashrc
