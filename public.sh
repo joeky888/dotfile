3a3f6da8887487c000cfdb9d9a974e3d80c1e44c
Joeky Zhan <jj16180339887@gmail.com>
2018-07-12 09:37:13 +0800

    Ignore case
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- public.sh
+++ public.sh
@@ -3,17 +3,17 @@
 # Transfer all personal settings to public
 # Usefull when I don't own this machine
 
-perl -C -Mutf8 -pe '/template/d' ~/dotfile/.gitconfig
-perl -C -Mutf8 -pe '/name/d'     ~/dotfile/.gitconfig
-perl -C -Mutf8 -pe '/email/d'    ~/dotfile/.gitconfig
+perl -C -Mutf8 -pie '/template/d' ~/dotfile/.gitconfig
+perl -C -Mutf8 -pie '/name/d'     ~/dotfile/.gitconfig
+perl -C -Mutf8 -pie '/email/d'    ~/dotfile/.gitconfig
 
 # sed -i $backup '/template/d'   ~/dotfile/.gitconfig
 # sed -i $backup '/name/d'       ~/dotfile/.gitconfig
 # sed -i $backup '/email/d'      ~/dotfile/.gitconfig
 # sed -i $backup "/pageup/Id"    ~/.vimrc
 # sed -i $backup "/pagedown/Id"  ~/.vimrc
-perl -C -Mutf8 -pe "/pageup/Id"    ~/.vimrc
-perl -C -Mutf8 -pe "/pagedown/Id"  ~/.vimrc
+perl -C -Mutf8 -pie "/pageup/id"    ~/.vimrc
+perl -C -Mutf8 -pie "/pagedown/id"  ~/.vimrc
 # sed -i '/zsh-hook/d'                        ~/dotfile/.bashrc
 # sed -i '/^\s*update_prompt\s*$/d'               ~/dotfile/.bashrc
 # sed -i 's/ZSH_THEME.*$/ZSH_THEME="bira"/'   ~/dotfile/.bashrc
