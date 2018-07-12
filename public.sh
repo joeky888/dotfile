70e07074bd3837c9da5ce902b97e1f32b02c1014
Joeky Zhan <jj16180339887@gmail.com>
2018-07-12 09:32:28 +0800

    Try perl
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- public.sh
+++ public.sh
@@ -3,16 +3,17 @@
 # Transfer all personal settings to public
 # Usefull when I don't own this machine
 
-backup=""
+perl -C -Mutf8 -pe '/template/d' ~/dotfile/.gitconfig
+perl -C -Mutf8 -pe '/name/d'     ~/dotfile/.gitconfig
+perl -C -Mutf8 -pe '/email/d'    ~/dotfile/.gitconfig
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  backup="''"
-fi
-sed -i $backup '/template/d'   ~/dotfile/.gitconfig
-sed -i $backup '/name/d'       ~/dotfile/.gitconfig
-sed -i $backup '/email/d'      ~/dotfile/.gitconfig
-sed -i $backup "/pageup/Id"    ~/.vimrc
-sed -i $backup "/pagedown/Id"  ~/.vimrc
+# sed -i $backup '/template/d'   ~/dotfile/.gitconfig
+# sed -i $backup '/name/d'       ~/dotfile/.gitconfig
+# sed -i $backup '/email/d'      ~/dotfile/.gitconfig
+# sed -i $backup "/pageup/Id"    ~/.vimrc
+# sed -i $backup "/pagedown/Id"  ~/.vimrc
+perl -C -Mutf8 -pe "/pageup/Id"    ~/.vimrc
+perl -C -Mutf8 -pe "/pagedown/Id"  ~/.vimrc
 # sed -i '/zsh-hook/d'                        ~/dotfile/.bashrc
 # sed -i '/^\s*update_prompt\s*$/d'               ~/dotfile/.bashrc
 # sed -i 's/ZSH_THEME.*$/ZSH_THEME="bira"/'   ~/dotfile/.bashrc
