05d3ba11c2aa7ad19b34c94e95a8393cf6124409
Joeky Zhan <jj16180339887@gmail.com>
2018-10-28 12:51:21 +0800

    Try bsd tar
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- install.sh
+++ install.sh
@@ -170,7 +170,7 @@ InstallAlpine()
   ALPINE_HOME="$HOME/Alpine"
   rm -rf $ALPINE_HOME && mkdir -p $ALPINE_HOME && cd $ALPINE_HOME
   aria2c $ALPINE_URL
-  proot --link2symlink -0 tar zxvf *.tar.gz
+  proot --link2symlink -0 bsdtar -xpf *.tar.gz
   cd -
 }
 
