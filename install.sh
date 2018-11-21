0f849b3510a36a52b621cd1dd35eb41ea3a71c8e
Joeky Zhan <jj16180339887@gmail.com>
2018-11-22 00:14:32 +0800

    Update mac apps
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com> <Joeky_Zhan@asus.com>

--- install.sh
+++ install.sh
@@ -268,17 +268,18 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then # Mac OSX
   export Home="$HOME"
   export SUDO=""
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  [ $(command -v brew) ] || /usr/bin/ruby -e "$(curl -fksSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew update
   brew install git vim coreutils ffmpeg aria2 tig tmux p7zip rsync
+  chsh -s $(command -v zsh) $(whoami)
   brew install mpv --with-bundle
   brew tap homebrew/cask
+  brew cask install hyper visual-studio-code
   brew tap homebrew/cask-fonts
-  brew tap homebrew/cask-drivers
-  brew cask install hyper
+  brew tap homebrew/cask-versions
+#   brew tap homebrew/cask-drivers
 #   brew cask install spectacle
 #   brew install mas
-  chsh -s $(command -v zsh) $(whoami)
 #   brew cask install macvim iterm2
 #   brew linkapps
   InstallMiniconda MacOSX
