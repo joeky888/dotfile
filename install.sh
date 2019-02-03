ecd81e7026deeb8e2c6e54ea31045a50acece77d
Joeky Zhan <jj16180339887@gmail.com>
2019-02-03 16:29:34 +0800

    No more powerpill
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com>

--- install.sh
+++ install.sh
@@ -212,7 +212,6 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     $SUDO sed -i '/\[options\]/a Color' /etc/pacman.conf
     $SUDO sed -i '/\[options\]/a XferCommand = aria2c -c -s16 -k1M -x16 --dir=/ -o %o %u' /etc/pacman.conf
     $SUDO app-fast -S vim tmux zsh curl bash-completion yay --noconfirm --needed
-#     [ -f /usr/bin/powerpill ] && $SUDO sed -i 's/^python3/\/usr\/bin\/python3/' $(which powerpill)
   else
     echo "Distro does not support at this moment."
     exit 1
