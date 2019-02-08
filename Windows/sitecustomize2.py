6c44d0c46fd310d853e6240befd3779aa4c1da16
Joeky Zhan <jj16180339887@gmail.com>
2019-02-08 09:43:06 +0800

    Python2 support
    Signed-off-by: Joeky Zhan <jj16180339887@gmail.com>

---
+++ Windows/sitecustomize2.py
@@ -0,0 +1,14 @@
+# encoding=utf8
+
+'''
+Put this file to python site-packages folder
+Check the site-packages folder location
+    $ python2
+    $ import site; site.getsitepackages()
+Put this file under site-packages folder
+'''
+
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
