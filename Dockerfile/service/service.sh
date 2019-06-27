56fe3bb143a09c09bdb24bacbd01fa0d39b2e9b5
Joeky Zhan <jj16180339887@gmail.com>
2019-06-27 10:41:23 +0800

    Mysql without password
    Signed-off-by: Joeky <jj16180339887@gmail.com>

--- Dockerfile/service/service.sh
+++ Dockerfile/service/service.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Mysql 5.7 root with password 123456
-docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+# Mysql 5.7 root without password
+docker run --name mysql -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # Mysql 5.7 with username libgo password 123456
 docker run --name mysql -p 3306:3306 -e MYSQL_USER=libgo -e MYSQL_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
