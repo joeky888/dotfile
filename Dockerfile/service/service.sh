089e24c4b96704358c7b3c1551a8447ee2529d6b
Joeky Zhan <jj16180339887@gmail.com>
2019-07-02 09:23:51 +0800

    Use spaces
    Signed-off-by: Joeky <jj16180339887@gmail.com>

--- Dockerfile/service/service.sh
+++ Dockerfile/service/service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Mysql 5.7 root with password 123456
-docker run --name mysql -p 3306:3306  --restart=always -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run --name mysql -p 3306:3306  --restart always -e MYSQL_ROOT_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # Mysql 5.7 root without password
-docker run --name mysql -p 3306:3306  --restart=always -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run --name mysql -p 3306:3306  --restart always -e MYSQL_ALLOW_EMPTY_PASSWORD=yes  -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # Mysql 5.7 with username libgo password 123456
-docker run --name mysql -p 3306:3306  --restart=always -e MYSQL_USER=libgo -e MYSQL_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run --name mysql -p 3306:3306  --restart always -e MYSQL_USER=libgo -e MYSQL_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # Redis latest
-docker run --name redis -p 6379:6379  --restart=always -d redis
+docker run --name redis -p 6379:6379  --restart always -d redis
