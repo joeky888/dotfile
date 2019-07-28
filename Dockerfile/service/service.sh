6f82ce6b9648cfa3c6e27f9a56c1c8fdf72909de
Joeky Zhan <jj16180339887@gmail.com>
2019-07-28 12:32:34 +0800

    Update pass
    Signed-off-by: Joeky <jj16180339887@gmail.com>

--- Dockerfile/service/service.sh
+++ Dockerfile/service/service.sh
@@ -10,9 +10,9 @@ docker run --name mysql -p 3306:3306 --restart always -e MYSQL_ALLOW_EMPTY_PASSW
 docker run --name mysql -p 3306:3306 --restart always -e MYSQL_USER=libgo -e MYSQL_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # Tidb root with password 123456
-docker run --name tidb -p 3306:4000 -p 10080:10080 --restart always -d pingcap/tidb:v3.0.1
-mycli --port 3306 --user root --default-character-set=utf8mb4
-UPDATE mysql.user SET Password=PASSWORD('MyNewPass') WHERE User='root';
+docker run --name tidb -p 3306:4000 -p 10080:10080 --restart always -d pingcap/tidb:v3.0.1 -store tikv
+mycli --port 3306 --user root
+UPDATE mysql.user SET Password=PASSWORD('123456') WHERE User='root';
 FLUSH PRIVILEGES;
 
 # Redis latest
