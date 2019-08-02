5305e070ac0d04d748cd204f312036e805377a86
Joeky Zhan <jj16180339887@gmail.com>
2019-08-02 18:48:18 +0800

    Remove tidb image tags
    Signed-off-by: Joeky <jj16180339887@gmail.com>

--- Dockerfile/service/service.sh
+++ Dockerfile/service/service.sh
@@ -10,7 +10,7 @@ docker run --name mysql -p 3306:3306 --restart always -e MYSQL_ALLOW_EMPTY_PASSW
 docker run --name mysql -p 3306:3306 --restart always -e MYSQL_USER=libgo -e MYSQL_PASSWORD=123456 -d mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 
 # Tidb root with password 123456
-docker run --name tidb -p 3306:4000 -p 10080:10080 --restart always -d pingcap/tidb:v3.0.1
+docker run --name tidb -p 3306:4000 -p 10080:10080 --restart always -d pingcap/tidb
 mycli --port 3306 --user root
 UPDATE mysql.user SET Password=PASSWORD('123456') WHERE User='root';
 FLUSH PRIVILEGES;
