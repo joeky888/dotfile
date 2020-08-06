788473f8b00bcda48c45cf6d5239fa82191b43a6
Joeky <joeky5888@gmail.com>
2020-08-06 16:15:41 +0800

    Set to 524288
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- Linux/optimize.sh
+++ Linux/optimize.sh
@@ -17,14 +17,15 @@ function upsert()
     fi
 }
 
-upsert "fs.file-max"                "fs.file-max = 200000"          /etc/sysctl.conf
-upsert "\*\s*soft\s*nofile"         "* soft  nofile  200000"        /etc/security/limits.conf
-upsert "\*\s*hard\s*nofile"         "* hard  nofile  200000"        /etc/security/limits.conf
-upsert "root\s*soft\s*nofile"       "root soft nofile 200000"       /etc/security/limits.conf
-upsert "root\s*hard\s*nofile"       "root hard nofile 200000"       /etc/security/limits.conf
-upsert "\*\s*soft\s*nproc"          "* soft  nproc  200000"         /etc/security/limits.conf
-upsert "\*\s*hard\s*nproc"          "* hard  nproc  200000"         /etc/security/limits.conf
-upsert "root\s*soft\s*nproc"        "root soft nproc 200000"        /etc/security/limits.conf
-upsert "root\s*hard\s*nproc"        "root hard nproc 200000"        /etc/security/limits.conf
+upsert "fs.file-max"                    "fs.file-max = 524288"                      /etc/sysctl.conf
+upsert "fs.inotify.max_user_watches"    "fs.inotify.max_user_watches = 524288"      /etc/sysctl.conf
+upsert "\*\s*soft\s*nofile"             "* soft  nofile  524288"                    /etc/security/limits.conf
+upsert "\*\s*hard\s*nofile"             "* hard  nofile  524288"                    /etc/security/limits.conf
+upsert "root\s*soft\s*nofile"           "root soft nofile 524288"                   /etc/security/limits.conf
+upsert "root\s*hard\s*nofile"           "root hard nofile 524288"                   /etc/security/limits.conf
+upsert "\*\s*soft\s*nproc"              "* soft  nproc  524288"                     /etc/security/limits.conf
+upsert "\*\s*hard\s*nproc"              "* hard  nproc  524288"                     /etc/security/limits.conf
+upsert "root\s*soft\s*nproc"            "root soft nproc 524288"                    /etc/security/limits.conf
+upsert "root\s*hard\s*nproc"            "root hard nproc 524288"                    /etc/security/limits.conf
 
 sysctl -p --system
