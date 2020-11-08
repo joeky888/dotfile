ed791d7890e0a794f03added46511843ce7921bd
Joeky <joeky5888@gmail.com>
2020-11-08 12:42:12 +0800

    Add VCS theme
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .config.fish
+++ .config.fish
@@ -1,4 +1,48 @@
 set fish_greeting # Disable fish messages on start
+
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_color_branch yellow --bold
+    set -g __fish_git_prompt_color_dirtystate red
+    set -g __fish_git_prompt_color_stagedstate red
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_untrackedfiles red
+    set -g __fish_git_prompt_color_cleanstate yellow --bold
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l suffix \n'>'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix \n'#'
+    end
+
+    # If we're running via SSH, change the host color.
+    set -l color_host $fish_color_host
+    if set -q SSH_TTY
+        set color_host $fish_color_host_remote
+    end
+
+    # Write pipestatus
+    # If the status was carried over (e.g. after `set`), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color $bold_flag $fish_color_status) $last_pipestatus)
+
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+end
+
 set -U fish_user_paths ~/dotfile/app-fast $fish_user_paths
 set -U fish_user_paths ~/dotfile/inxi $fish_user_paths
 set -U fish_user_paths ~/dotfile/diff-highlight $fish_user_paths
