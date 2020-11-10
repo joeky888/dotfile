2cead89890fe5376908fc15a8b25d86db73cc534
Joeky <joeky5888@gmail.com>
2020-11-10 11:07:21 +0800

    Clean up
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .config.fish
+++ .config.fish
@@ -34,23 +34,9 @@ function fish_prompt --description 'Write out the prompt'
         set suffix \n'#'
     end
 
-    # If we're running via SSH, change the host color.
-    set -l color_host $fish_color_host
-#     if set -q SSH_TTY
-#         set color_host $fish_color_host_remote
-#     end
 
-    # Write pipestatus
-    # If the status was carried over (e.g. after `set`), don't bold it.
-#     set -l bold_flag --bold
-#     set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
-#     if test $__fish_prompt_status_generation = $status_generation
-#         set bold_flag
-#     end
-#     set __fish_prompt_status_generation $status_generation
-#     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color $bold_flag $fish_color_status) $last_pipestatus)
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
 end
 
 set -U fish_user_paths ~/dotfile/app-fast $fish_user_paths
