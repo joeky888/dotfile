05b97ba8b406850d6cad0c54517acc09454c5f8d
Joeky <joeky5888@gmail.com>
2020-11-17 10:18:24 +0800

    Don't do a full upgrade
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- install.sh
+++ install.sh
@@ -380,7 +380,7 @@ elif [[ "$OSTYPE" == "linux-android" ]]; then # Android Termux
   apt install -y --assume-yes git aria2
   InstallDotfile
   # cd ~/dotfile/app-fast && git reset --hard
-  ~/dotfile/app-fast/app-fast full-upgrade -y
+  # ~/dotfile/app-fast/app-fast full-upgrade -y
   ~/dotfile/app-fast/app-fast install -y man vim git tig zsh fish tmux curl aria2 file tar wget bash-completion htop openssh mosh grep sed gawk ffmpeg p7zip
   # ~/dotfile/app-fast/app-fast install -y clang autoconf automake bison bzip2 util-linux cmake coreutils diffutils flex gzip make patch perl silversearcher-ag
   # ~/dotfile/app-fast/app-fast install -y libtool ncurses-utils python-dev libffi-dev libcrypt-dev openssl-dev readline-dev
