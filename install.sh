31144a5b4b8a55b11603eac402d2b602ef009fd4
Joeky <joeky5888@gmail.com>
2020-11-18 11:46:00 +0800

    Don't set default shell to fish
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- install.sh
+++ install.sh
@@ -385,7 +385,7 @@ elif [[ "$OSTYPE" == "linux-android" ]]; then # Android Termux
   # ~/dotfile/app-fast/app-fast install -y clang autoconf automake bison bzip2 util-linux cmake coreutils diffutils flex gzip make patch perl silversearcher-ag
   # ~/dotfile/app-fast/app-fast install -y libtool ncurses-utils python-dev libffi-dev libcrypt-dev openssl-dev readline-dev
   # InstallPIP
-  chsh -s fish
+  # chsh -s fish
   # ~/dotfile/app-fast/app-fast install -y proot bsdtar
   # InstallAlpine
 
