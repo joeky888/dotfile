e88fe3998e290e5df5eabd8fc3cfbc8abb568bf7
Joeky <joeky5888@gmail.com>
2021-05-17 10:42:43 +0800

    Update lint
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- Programming/rust-playground/src/main.rs
+++ Programming/rust-playground/src/main.rs
@@ -1,3 +1,15 @@
+#![forbid(unsafe_code)]
+#![deny(unused_imports)]
+#![deny(unused_must_use)]
+#![deny(dead_code)]
+#![deny(clippy::all, clippy::perf, clippy::nursery, clippy::pedantic)]
+#![deny(clippy::filetype_is_file)]
+#![deny(clippy::cargo)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::match_like_matches_macro)]
+#![deny(clippy::needless_update)]
+
 mod environment;
 mod logger;
 
