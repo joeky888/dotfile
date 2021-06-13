dc4c913c413b0e4075a3395fb32d26a4923576a6
Joeky <joeky5888@gmail.com>
2021-06-13 17:04:01 +0800

    Use TMPDIR
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .config.fish
+++ .config.fish
@@ -56,18 +56,18 @@ function fish_prompt --description 'Write out the prompt'
   set color_status green
   set print_status "✓"
   if [ "$last_pipestatus" != "0" ]
-    set color_status red
-    set print_status "$last_pipestatus"
+  set color_status red
+  set print_status "$last_pipestatus"
   end
 
   # Color the prompt differently when we're root
   set -l color_cwd $fish_color_cwd
   set -l suffix \n'> '
   if functions -q fish_is_root_user; and fish_is_root_user
-    if set -q fish_color_cwd_root
-      set color_cwd $fish_color_cwd_root
-    end
-    set suffix \n'# '
+  if set -q fish_color_cwd_root
+    set color_cwd $fish_color_cwd_root
+  end
+  set suffix \n'# '
   end
 
   echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal (set_color $color_status) " $print_status" $normal $suffix
@@ -77,6 +77,11 @@ status is-interactive
 or exit 0
 
 set -l xdg_runtime_dir "$XDG_RUNTIME_DIR"
+
+# Use tmpdir for termux
+test -z "$xdg_runtime_dir"
+and set xdg_runtime_dir "$TMPDIR"
+
 test -z "$xdg_runtime_dir"
 and set xdg_runtime_dir /tmp
 
@@ -85,120 +90,120 @@ mkdir -p $__async_prompt_tmpdir
 
 # Setup after the user defined prompt functions are loaded.
 function __async_prompt_setup_on_startup --on-event fish_prompt
-    functions -e (status current-function)
+  functions -e (status current-function)
 
-    for func in (__async_prompt_config_functions)
-        function $func -V func
-            test -e $__async_prompt_tmpdir'/'$fish_pid'_'$func
-            and cat $__async_prompt_tmpdir'/'$fish_pid'_'$func
-        end
+  for func in (__async_prompt_config_functions)
+    function $func -V func
+      test -e $__async_prompt_tmpdir'/'$fish_pid'_'$func
+      and cat $__async_prompt_tmpdir'/'$fish_pid'_'$func
     end
+  end
 end
 
 function __async_prompt_fire --on-event fish_prompt
-    set st $status
+  set st $status
 
-    for func in (__async_prompt_config_functions)
-        set -l tmpfile $__async_prompt_tmpdir'/'$fish_pid'_'$func
+  for func in (__async_prompt_config_functions)
+    set -l tmpfile $__async_prompt_tmpdir'/'$fish_pid'_'$func
 
-        if functions -q $func'_loading_indicator' && test -e $tmpfile
-            read -zl last_prompt <$tmpfile
-            eval (string escape -- $func'_loading_indicator' "$last_prompt") >$tmpfile
-        end
-
-        __async_prompt_config_inherit_variables | __async_prompt_spawn $st \
-            $func' | read -z prompt
-            echo -n $prompt >'$tmpfile
+    if functions -q $func'_loading_indicator' && test -e $tmpfile
+      read -zl last_prompt <$tmpfile
+      eval (string escape -- $func'_loading_indicator' "$last_prompt") >$tmpfile
     end
+
+    __async_prompt_config_inherit_variables | __async_prompt_spawn $st \
+      $func' | read -z prompt
+      echo -n $prompt >'$tmpfile
+  end
 end
 
 function __async_prompt_spawn
-    set -l envs
-    begin
-        set st $argv[1]
-        while read line
-            switch "$line"
-                case FISH_VERSION PWD _ history 'fish_*' hostname version
-                case status
-                    echo status $st
-                case SHLVL
-                    set envs $envs SHLVL=(math $SHLVL - 1)
-                case '*'
-                    echo $line (string escape -- $$line)
-            end
-        end
-    end | read -lz vars
-    echo $vars | env $envs fish -c '
-    function __async_prompt_set_status
-        return $argv
+  set -l envs
+  begin
+    set st $argv[1]
+    while read line
+      switch "$line"
+        case FISH_VERSION PWD _ history 'fish_*' hostname version
+        case status
+          echo status $st
+        case SHLVL
+          set envs $envs SHLVL=(math $SHLVL - 1)
+        case '*'
+          echo $line (string escape -- $$line)
+      end
     end
-    function __async_prompt_signal
-        kill -s "'(__async_prompt_config_internal_signal)'" '$fish_pid'
+  end | read -lz vars
+  echo $vars | env $envs fish -c '
+  function __async_prompt_set_status
+    return $argv
+  end
+  function __async_prompt_signal
+    kill -s "'(__async_prompt_config_internal_signal)'" '$fish_pid'
+  end
+  while read -a line
+    test -z "$line"
+    and continue
+    if test "$line[1]" = status
+      set st $line[2]
+    else
+      eval set "$line"
     end
-    while read -a line
-        test -z "$line"
-        and continue
-        if test "$line[1]" = status
-            set st $line[2]
-        else
-            eval set "$line"
-        end
-    end
-    not set -q st
-    and true
-    or __async_prompt_set_status $st
-    '$argv[2]'
-    __async_prompt_signal
-    sleep 0.3
-    __async_prompt_signal
-    sleep 0.3
-    __async_prompt_signal' &
-    disown
+  end
+  not set -q st
+  and true
+  or __async_prompt_set_status $st
+  '$argv[2]'
+  __async_prompt_signal
+  sleep 0.3
+  __async_prompt_signal
+  sleep 0.3
+  __async_prompt_signal' &
+  disown
 end
 
 function __async_prompt_config_inherit_variables
-    if set -q async_prompt_inherit_variables
-        if test "$async_prompt_inherit_variables" = all
-            set -ng
-        else
-            for item in $async_prompt_inherit_variables
-                echo $item
-            end
-        end
+  if set -q async_prompt_inherit_variables
+    if test "$async_prompt_inherit_variables" = all
+      set -ng
     else
-        echo status
-        echo SHLVL
-        echo CMD_DURATION
+      for item in $async_prompt_inherit_variables
+        echo $item
+      end
     end
+  else
+    echo status
+    echo SHLVL
+    echo CMD_DURATION
+  end
 end
 
 function __async_prompt_config_functions
-    set -l funcs (
-        if set -q $async_prompt_functions
-            string join \n $async_prompt_functions
-        else
-            echo fish_prompt
-            echo fish_right_prompt
-        end
-    )
-    for func in $funcs
-        functions -q "$func"
-        or continue
-
-        echo $func
+  set -l funcs (
+    if set -q $async_prompt_functions
+      string join \n $async_prompt_functions
+    else
+      echo fish_prompt
+      echo fish_right_prompt
     end
+  )
+  for func in $funcs
+    functions -q "$func"
+    or continue
+
+    echo $func
+  end
 end
 
 function __async_prompt_config_internal_signal
-    if test -z "$async_prompt_signal_number"
-        echo SIGUSR1
-    else
-        echo "$async_prompt_signal_number"
-    end
+  if test -z "$async_prompt_signal_number"
+    echo SIGUSR1
+  else
+    echo "$async_prompt_signal_number"
+  end
 end
 
 function __async_prompt_repaint_prompt --on-signal (__async_prompt_config_internal_signal)
-    commandline -f repaint >/dev/null 2>/dev/null
+  commandline -f repaint >/dev/null 2>/dev/null
 end
 
 # Enable async prompt
