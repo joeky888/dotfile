5891e2372edc7b612d517400a7eeaef3c4a7356a
Joeky <joeky5888@gmail.com>
2021-06-26 20:10:45 +0800

    Disable hyper link rules
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,4 +7,11 @@ return {
     -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
     {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}},
   },
+  hyperlink_rules = {
+    -- Disable clickable hyper links
+    {
+      regex = "",
+      format = "",
+    },
+  },
 }
