dd0e336b648f7e80b26fdfe0ae42f3f233d3876d
Joeky <joeky5888@gmail.com>
2021-06-26 20:14:01 +0800

    Set monokai theme
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
   default_prog = {"powershell.exe"},
+  color_scheme = "Molokai",
   keys = {
     {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
     -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
