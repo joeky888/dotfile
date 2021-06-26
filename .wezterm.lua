5ad74ca7ef93fd4c7dac880cf5d2cd4c280d82ec
Joeky <joeky5888@gmail.com>
2021-06-26 13:31:06 +0800

    Switching between tabs in wezterm
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,3 +1,10 @@
+local wezterm = require 'wezterm';
+
 return {
   default_prog = {"powershell.exe"},
+  keys = {
+    {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}},
+  },
 }
