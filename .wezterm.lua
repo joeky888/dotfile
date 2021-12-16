763674f9d1b2d7e030197ab5f27938236d13ced2
Joeky <joeky5888@gmail.com>
2021-12-16 11:19:02 +0800

    Set opacity
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require 'wezterm';
 return {
   default_prog = {"powershell.exe"},
   color_scheme = "Molokai",
+  window_background_opacity = 0.8,
   keys = {
     {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
     -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
