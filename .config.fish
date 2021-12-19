f4dbb34d1ea8ee44fceb23379a26e40b715c2de4
Joeky <joeky5888@gmail.com>
2021-12-19 22:59:18 +0800

    Add grc for fish
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .config.fish
+++ .config.fish
@@ -258,3 +258,25 @@ end
 
 type -q helm; and helm completion fish 2>/dev/null | source
 type -q kubectl; and kubectl completion fish | source
+
+set -U grc_plugin_execs cat cvs df diff dig gcc g++ ifconfig \
+       make mount mtr netstat ping ps tail traceroute \
+       wdiff blkid du dnf docker docker-compose docker-machine id ip iostat journalctl kubectl \
+       last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
+       fdisk findmnt free semanage sar ss sysctl systemctl stat showmount \
+       tcpdump tune2fs vmstat w who sockstat
+
+if type -q grc
+  for executable in $grc_plugin_execs
+    if type -q $executable
+      function $executable --inherit-variable executable --wraps=$executable
+        if isatty 1
+          grc $executable $argv
+        else
+          eval command $executable $argv
+        end
+      end
+    end
+  end
+end
+
