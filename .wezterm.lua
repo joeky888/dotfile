81c2f6531ac322c96804ac86f715c556cdee9547
Joeky <joeky5888@gmail.com>
2022-06-13 00:21:56 +0800

    Split window
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,11 +4,42 @@ return {
   default_prog = {"powershell.exe"},
   color_scheme = "Molokai",
   window_background_opacity = 0.8,
+  leader = { key="b", mods="CTRL", timeout_milliseconds=1500 }, -- Like tmux ctrl-b
   keys = {
-    {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=-1}},
-    {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ActivateTabRelative=1}},
-    {key="u", mods="CTRL", action=wezterm.action{ScrollByPage=-0.5}},
-    {key="j", mods="CTRL", action=wezterm.action{ScrollByPage=0.5}},
+    {key="LeftArrow", mods="CTRL|SHIFT", action=wezterm.action{ ActivateTabRelative=-1} }, -- Move to left tab
+    {key="RightArrow", mods="CTRL|SHIFT", action=wezterm.action{ ActivateTabRelative=1} }, -- Move to right tab
+    {key="u", mods="CTRL", action=wezterm.action{ ScrollByPage=-0.5 }}, -- Scroll up
+    {key="j", mods="CTRL", action=wezterm.action{ ScrollByPage=0.5 }}, -- Scroll down
+    {
+      key="_", mods="LEADER|SHIFT", action=wezterm.action{
+        SplitVertical={
+          domain="CurrentPaneDomain",
+        },
+      },
+    }, -- Split Horizontally
+    {
+      key="|", mods="LEADER|SHIFT", action=wezterm.action{
+        SplitHorizontal={
+          domain="CurrentPaneDomain",
+        },
+      },
+    }, -- Split Vertically
+    {
+      key="+", mods="LEADER|SHIFT", action={
+        Multiple={
+          {
+            SplitHorizontal={
+              domain="CurrentPaneDomain",
+            },
+          },
+          {
+            SplitVertical={
+              domain="CurrentPaneDomain",
+            },
+          },
+        }
+      }
+    }, -- TODO: Split Horizontally&&Horizontally
   },
   hyperlink_rules = {
     -- Disable clickable hyper links
