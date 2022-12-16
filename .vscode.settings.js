9f155a82d23911cffc54036f39426cd56e87b7aa
Joeky <joeky5888@gmail.com>
2022-12-16 15:16:41 +0800

    Enable multi tab
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .vscode.settings.js
+++ .vscode.settings.js
@@ -19,6 +19,7 @@
     "editor.cursorBlinking": "solid",
     "telemetry.telemetryLevel": "off",
     "editor.semanticHighlighting.enabled": true,
+    "window.nativeTabs": true,
     "vscode_custom_css.imports": [
         "https://raw.githubusercontent.com/joeky888/dotfile/master/.vscode.monokai-neon-glow.css"
     ],
