1c99e0e0f9b919913e83a242e4bb34c1f55cfff5
Joeky <joeky5888@gmail.com>
2023-04-09 06:46:33 +0800

    And editor
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .vscode.settings.js
+++ .vscode.settings.js
@@ -1,6 +1,6 @@
 {
     "editor.fontSize": 20,
-    "editor.fontFamily": "'Ubuntu Mono', 'Droid Sans Fallback', 'Sarasa Mono TC', 'Consolas', 'NSimsun', monospace",
+    "editor.fontFamily": "'UbuntuMono Nerd Font', 'Ubuntu Mono', 'Droid Sans Fallback', 'Sarasa Mono TC', 'Consolas', 'NSimsun', monospace",
     "terminal.integrated.fontFamily": "'UbuntuMono Nerd Font', 'Iosevka', 'Ubuntu Mono', 'Droid Sans Fallback', 'Sarasa Mono TC', 'Consolas', 'NSimsun', 'Monaco', monospace",
     "editor.wordWrap": "off",
     "editor.tabSize": 2,
