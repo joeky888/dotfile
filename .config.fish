4da4acb2fcdacded64913b70e54a20a9d76d5a23
Joeky <joeky5888@gmail.com>
2023-08-03 20:45:41 +0800

    Use zsh
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .config.fish
+++ .config.fish
@@ -20,203 +20,209 @@ bind \cr history-token-search-backward
 # bind \cZ undo # Available in fish >= 3.2.0
 # bind \cY redo # Available in fish >= 3.2.0
 
-set fish_prompt_pwd_dir_length 0 # Show full path on the prompt
 set -U fish_user_paths ""
 set -U fish_user_paths $HOME/dotfile/app-fast $fish_user_paths
 set -U fish_user_paths $HOME/dotfile/inxi $fish_user_paths
 set -U fish_user_paths $HOME/dotfile/diff-highlight $fish_user_paths
 
-# For command line highlighting
-set fish_color_command green --bold
-set fish_color_comment black --bold
-
-# For prompt
-set fish_color_cwd cyan --bold
-set fish_color_cwd_root red --bold
-set fish_color_user red --bold
-set fish_color_host green --bold
-set fish_color_quote bryellow
-set fish_color_comment brblack
-set -g __fish_git_prompt_showdirtystate 1 # This takes too much time
-set -g __fish_git_prompt_showuntrackedfiles 1
-set -g __fish_git_prompt_color_branch yellow --bold
-set -g __fish_git_prompt_color_dirtystate red # This takes too much time
-set -g __fish_git_prompt_color_stagedstate red
-set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles red
-set -g __fish_git_prompt_color_cleanstate yellow --bold
-
 if test -f /opt/homebrew/bin/brew
   eval (/opt/homebrew/bin/brew shellenv)
 end
 
-# Aync git promt
-# Ref: https://github.com/pure-fish/pure/wiki/Async-git-Prompt
-# Ref: https://github.com/acomagu/fish-async-prompt
+if type -q starship
+  starship init fish | source
+else
+  set fish_prompt_pwd_dir_length 0 # Show full path on the prompt
 
-function fish_prompt --description 'Write out the prompt'
-  set -l last_pipestatus $pipestatus
-  set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-  set -l normal (set_color normal)
-  set -l red (set_color red --bold)
-  set -l yellow (set_color yellow --bold)
-  set -l green (set_color green --bold)
+  # For command line highlighting
+  set fish_color_command green --bold
+  set fish_color_comment black --bold
 
-  set color_status green
-  set print_status "✓"
-  if [ "$last_pipestatus" -ne "0" ]
-    set color_status red
-    set print_status "$last_pipestatus"
-  end
+  # For prompt
+  set fish_color_cwd cyan --bold
+  set fish_color_cwd_root red --bold
+  set fish_color_user red --bold
+  set fish_color_host green --bold
+  set fish_color_quote bryellow
+  set fish_color_comment brblack
+  set -g __fish_git_prompt_showdirtystate 1 # This takes too much time
+  set -g __fish_git_prompt_showuntrackedfiles 1
+  set -g __fish_git_prompt_color_branch yellow --bold
+  set -g __fish_git_prompt_color_dirtystate red # This takes too much time
+  set -g __fish_git_prompt_color_stagedstate red
+  set -g __fish_git_prompt_color_invalidstate red
+  set -g __fish_git_prompt_color_untrackedfiles red
+  set -g __fish_git_prompt_color_cleanstate yellow --bold
 
-  # Color the prompt differently when we're root
-  # if functions -q fish_is_root_user; and fish_is_root_user
-  if test (id -u) -eq 0
-    set color_cwd $fish_color_cwd_root
-    set suffix '#'
-  else
-    set color_cwd $fish_color_cwd
-    set suffix '>'
-  end
 
-  echo -n -s (set_color $fish_color_user)"$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal (set_color $color_status) " $print_status" $normal\n$red$suffix$yellow$suffix$green$suffix' '
-end
+  # Aync git promt
+  # Ref: https://github.com/pure-fish/pure/wiki/Async-git-Prompt
+  # Ref: https://github.com/acomagu/fish-async-prompt
 
-status is-interactive
-or exit 0
+  function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+    set -l normal (set_color normal)
+    set -l red (set_color red --bold)
+    set -l yellow (set_color yellow --bold)
+    set -l green (set_color green --bold)
 
-set -g __async_prompt_tmpdir "$XDG_RUNTIME_DIR"
-test -z "$__async_prompt_tmpdir"
-and set -g __async_prompt_tmpdir "$TMPDIR"
-
-test -z "$__async_prompt_tmpdir"
-and set -g __async_prompt_tmpdir "$TMP"
-
-test -z "$__async_prompt_tmpdir"
-and set -g __async_prompt_tmpdir (command mktemp -d)
-
-mkdir -p $__async_prompt_tmpdir
-
-# Setup after the user defined prompt functions are loaded.
-function __async_prompt_setup_on_startup --on-event fish_prompt
-  functions -e (status current-function)
-
-  for func in (__async_prompt_config_functions)
-    function $func -V func
-      test -e $__async_prompt_tmpdir'/'$fish_pid'_'$func
-      and cat $__async_prompt_tmpdir'/'$fish_pid'_'$func
-    end
-  end
-end
-
-function __async_prompt_fire --on-event fish_prompt
-  set st $status
-
-  for func in (__async_prompt_config_functions)
-    set -l tmpfile $__async_prompt_tmpdir'/'$fish_pid'_'$func
-
-    if functions -q $func'_loading_indicator' && test -e $tmpfile
-      read -zl last_prompt <$tmpfile
-      eval (string escape -- $func'_loading_indicator' "$last_prompt") >$tmpfile
+    set color_status green
+    set print_status "✓"
+    if [ "$last_pipestatus" -ne "0" ]
+      set color_status red
+      set print_status "$last_pipestatus"
     end
 
-    __async_prompt_config_inherit_variables | __async_prompt_spawn $st \
-      $func' | read -z prompt
-      echo -n $prompt >'$tmpfile
-  end
-end
+    # Color the prompt differently when we're root
+    # if functions -q fish_is_root_user; and fish_is_root_user
+    if test (id -u) -eq 0
+      set color_cwd $fish_color_cwd_root
+      set suffix '#'
+    else
+      set color_cwd $fish_color_cwd
+      set suffix '>'
+    end
 
-function __async_prompt_spawn
-  set -l envs
-  begin
-    set st $argv[1]
-    while read line
-      switch "$line"
-        case FISH_VERSION PWD _ history 'fish_*' hostname version
-        case status
-          echo status $st
-        case SHLVL
-          set envs $envs SHLVL=(math $SHLVL - 1)
-        case '*'
-          echo $line (string escape -- $$line)
+    echo -n -s (set_color $fish_color_user)"$USER" $normal @ (set_color $fish_color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal (set_color $color_status) " $print_status" $normal\n$red$suffix$yellow$suffix$green$suffix' '
+  end
+
+  status is-interactive
+  or exit 0
+
+  set -g __async_prompt_tmpdir "$XDG_RUNTIME_DIR"
+  test -z "$__async_prompt_tmpdir"
+  and set -g __async_prompt_tmpdir "$TMPDIR"
+
+  test -z "$__async_prompt_tmpdir"
+  and set -g __async_prompt_tmpdir "$TMP"
+
+  test -z "$__async_prompt_tmpdir"
+  and set -g __async_prompt_tmpdir (command mktemp -d)
+
+  mkdir -p $__async_prompt_tmpdir
+
+  # Setup after the user defined prompt functions are loaded.
+  function __async_prompt_setup_on_startup --on-event fish_prompt
+    functions -e (status current-function)
+
+    for func in (__async_prompt_config_functions)
+      function $func -V func
+        test -e $__async_prompt_tmpdir'/'$fish_pid'_'$func
+        and cat $__async_prompt_tmpdir'/'$fish_pid'_'$func
       end
     end
-  end | read -lz vars
-  echo $vars | env $envs fish -c '
-  function __async_prompt_set_status
-    return $argv
   end
-  function __async_prompt_signal
-    kill -s "'(__async_prompt_config_internal_signal)'" '$fish_pid'
-  end
-  while read -a line
-    test -z "$line"
-    and continue
-    if test "$line[1]" = status
-      set st $line[2]
-    else
-      eval set "$line"
+
+  function __async_prompt_fire --on-event fish_prompt
+    set st $status
+
+    for func in (__async_prompt_config_functions)
+      set -l tmpfile $__async_prompt_tmpdir'/'$fish_pid'_'$func
+
+      if functions -q $func'_loading_indicator' && test -e $tmpfile
+        read -zl last_prompt <$tmpfile
+        eval (string escape -- $func'_loading_indicator' "$last_prompt") >$tmpfile
+      end
+
+      __async_prompt_config_inherit_variables | __async_prompt_spawn $st \
+        $func' | read -z prompt
+        echo -n $prompt >'$tmpfile
     end
   end
-  not set -q st
-  and true
-  or __async_prompt_set_status $st
-  '$argv[2]'
-  __async_prompt_signal
-  sleep 0.3
-  __async_prompt_signal
-  sleep 0.3
-  __async_prompt_signal' &
-  disown
-end
 
-function __async_prompt_config_inherit_variables
-  if set -q async_prompt_inherit_variables
-    if test "$async_prompt_inherit_variables" = all
-      set -ng
-    else
-      for item in $async_prompt_inherit_variables
-        echo $item
+  function __async_prompt_spawn
+    set -l envs
+    begin
+      set st $argv[1]
+      while read line
+        switch "$line"
+          case FISH_VERSION PWD _ history 'fish_*' hostname version
+          case status
+            echo status $st
+          case SHLVL
+            set envs $envs SHLVL=(math $SHLVL - 1)
+          case '*'
+            echo $line (string escape -- $$line)
+        end
+      end
+    end | read -lz vars
+    echo $vars | env $envs fish -c '
+    function __async_prompt_set_status
+      return $argv
+    end
+    function __async_prompt_signal
+      kill -s "'(__async_prompt_config_internal_signal)'" '$fish_pid'
+    end
+    while read -a line
+      test -z "$line"
+      and continue
+      if test "$line[1]" = status
+        set st $line[2]
+      else
+        eval set "$line"
       end
     end
-  else
-    echo status
-    echo SHLVL
-    echo CMD_DURATION
+    not set -q st
+    and true
+    or __async_prompt_set_status $st
+    '$argv[2]'
+    __async_prompt_signal
+    sleep 0.3
+    __async_prompt_signal
+    sleep 0.3
+    __async_prompt_signal' &
+    disown
   end
-end
 
-function __async_prompt_config_functions
-  set -l funcs (
-    if set -q $async_prompt_functions
-      string join \n $async_prompt_functions
+  function __async_prompt_config_inherit_variables
+    if set -q async_prompt_inherit_variables
+      if test "$async_prompt_inherit_variables" = all
+        set -ng
+      else
+        for item in $async_prompt_inherit_variables
+          echo $item
+        end
+      end
     else
-      echo fish_prompt
-      echo fish_right_prompt
+      echo status
+      echo SHLVL
+      echo CMD_DURATION
     end
-  )
-  for func in $funcs
-    functions -q "$func"
-    or continue
-
-    echo $func
   end
-end
 
-function __async_prompt_config_internal_signal
-  if test -z "$async_prompt_signal_number"
-    echo SIGUSR1
-  else
-    echo "$async_prompt_signal_number"
+  function __async_prompt_config_functions
+    set -l funcs (
+      if set -q $async_prompt_functions
+        string join \n $async_prompt_functions
+      else
+        echo fish_prompt
+        echo fish_right_prompt
+      end
+    )
+    for func in $funcs
+      functions -q "$func"
+      or continue
+
+      echo $func
+    end
   end
-end
 
-function __async_prompt_repaint_prompt --on-signal (__async_prompt_config_internal_signal)
-  commandline -f repaint >/dev/null 2>/dev/null
-end
+  function __async_prompt_config_internal_signal
+    if test -z "$async_prompt_signal_number"
+      echo SIGUSR1
+    else
+      echo "$async_prompt_signal_number"
+    end
+  end
 
-# Enable async prompt
-set async_prompt_functions _pure_prompt_git
+  function __async_prompt_repaint_prompt --on-signal (__async_prompt_config_internal_signal)
+    commandline -f repaint >/dev/null 2>/dev/null
+  end
+
+  # Enable async prompt
+  set async_prompt_functions _pure_prompt_git
+end
 
 set DL_ARGUMENTS "-o '%(title)s.%(ext)s' --write-sub --all-subs --embed-subs --hls-prefer-native --no-check-certificate --ignore-errors"
 set PLAYER_ARGUMENTS "--cache=yes --cache-dir=/tmp --cache-on-disk=yes --ytdl-raw-options=no-check-certificate=,yes-playlist=,hls-prefer-native=,ignore-errors=,write-auto-sub=,write-sub=,sub-lang='(en|zh).*'"
