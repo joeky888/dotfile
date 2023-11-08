434066a479435bc01a943efb77ffdffe9fb64aba
Joeky <joeky5888@gmail.com>
2023-11-09 02:11:02 +0800

    Extract audio
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .config.fish
+++ .config.fish
@@ -243,6 +243,7 @@ alias l='ls -lah'
 alias aria2c="aria2c $DOWNLOADER_ARGUMENTS"
 alias aria2c-bt-qBittorrent="aria2c $DOWNLOADER_ARGUMENTS $TORRENT_ARGUMENTS --user-agent='qBittorrent/4.1.1' --peer-id-prefix='-qB4110-' --bt-tracker=(curl -s https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | tr -s '\n' | tr '\n' ',')"
 alias yt-dlp="yt-dlp $DL_ARGUMENTS"
+alias yt-dlp-audio='yt-dlp --extract-audio'
 alias yt-dlp-aria2c="yt-dlp $DL_ARGUMENTS --external-downloader aria2c --external-downloader-args '$DOWNLOADER_ARGUMENTS'"
 alias yt-dlp-480="yt-dlp -f 'bestvideo[height<=480][fps<=30][vcodec!^=av01]+bestaudio/best'"
 alias yt-dlp-720="yt-dlp -f 'bestvideo[height<=720][fps<=30][vcodec!^=av01]+bestaudio/best'"
