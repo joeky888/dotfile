db53b3002a6518d4d6ca837c2985216d8e15d1d5
Joeky <joeky5888@gmail.com>
2025-02-15 19:04:52 +0800

    Use ruff for zed
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- .zed.settings.js
+++ .zed.settings.js
@@ -41,6 +41,23 @@
         "!eslint",
         "tailwindcss-language-server"
       ]
+    },
+    "Python": {
+      "language_servers": ["ruff"],
+      "format_on_save": "on",
+      "formatter": [
+        {
+          "code_actions": {
+            "source.organizeImports.ruff": true,
+            "source.fixAll.ruff": true
+          }
+        },
+        {
+          "language_server": {
+            "name": "ruff"
+          }
+        }
+      ]
     }
   }
 }
